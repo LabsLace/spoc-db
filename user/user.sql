fe8bb0baf4880839bd0c2119fc174174f5b56b7b
macmaad <rod.mac.mad@gmail.com>
2021-12-27 14:10:03 -0600

    fix types

--- user/user.sql
+++ user/user.sql
@@ -2,21 +2,19 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE SCHEMA users;
 
-
 CREATE TABLE IF NOT EXISTS users.user_information(
     id SERIAL NOT NULL,
     user_uuid VARCHAR(36) NOT NULL UNIQUE,
     username VARCHAR(60) NOT NULL UNIQUE,
     first_name VARCHAR(60) NOT NULL,
     last_name VARCHAR(60) NOT NULL, 
-    email VARCHAR(60) NOT NULL,
-    salt varchar(32) NOT NULL,
-    password_hash VARCHAR(260) NOT NULL,
+    email VARCHAR(60) NOT NULL UNIQUE,
+    salt BYTEA NOT NULL,
+    password_hash BYTEA NOT NULL,
     is_active BOOLEAN NOT NULL DEFAULT TRUE,
     created_tms TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     edited_tms TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE UNIQUE INDEX IF NOT EXISTS user_uuid_index ON users.user_information(user_uuid);
-
 
